03c8aea1934eda8c216dc95462478654377e0254
Brunno Bocardo <105675486+Brunno-Bocardo@users.noreply.github.com>
2023-06-27 09:17:27 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,35 +1,32 @@
 // POPUP
-const enviar = document.getElementById("btn-enviar")
-const fechar = document.getElementById("btn-close")
-const modal = document.querySelector("dialog")
+const enviar = document.getElementById("btn-enviar");
+const fechar = document.getElementById("btn-close");
+const modal = document.querySelector("dialog");
+const invalido = document.getElementById("btn-invalido")
+
 enviar.onclick = function (event) {
-    event.preventDefault();
-
-    const nome = document.getElementById("nome").value;
-    const planeta = document.getElementById("escolha").options[document.getElementById("escolha").selectedIndex].text;
-
-    document.getElementById("pop-nome").textContent = nome;
-    document.getElementById("pop-planeta").textContent = planeta;
-
-    modal.showModal()
+  event.preventDefault();
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const telefone = document.getElementById("telefone").value;
+  const senha = document.getElementById("senha").value;
+  const planeta =
+    document.getElementById("escolha").options[
+      document.getElementById("escolha").selectedIndex
+    ].text;
+  document.getElementById("pop-nome").textContent = nome;
+  document.getElementById("pop-planeta").textContent = planeta;
+  document.getElementById("pop-email").textContent = email;
+  document.getElementById("pop-telefone").textContent = telefone;
+  document.getElementById("pop-senha").textContent = senha;
+  modal.showModal();
 };
+
 fechar.onclick = function () {
-    modal.close()
-    document.querySelector("form").submit();
+  modal.close();
+  document.querySelector("form").submit();
 };
 
-
-// FAQ
-const abrir = document.querySelectorAll(".item");
-
-abrir.forEach(function (item) {
-  item.addEventListener("click", function () {
-    console.log("aaa")
-    const itemAtivoAtual = document.querySelector(".ativo");
-
-    if (itemAtivoAtual) {
-      itemAtivoAtual.classList.remove("ativo");
-    }
-    item.classList.add("ativo");
-  });
-});
+invalido.onclick = function () {
+  modal.close();
+};
